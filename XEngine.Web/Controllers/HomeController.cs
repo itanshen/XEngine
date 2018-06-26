b815b95ee74e4b042d96fcbdd8c4800c4cb542d1
itanshen <tanshen@126.com>
2018-06-26 11:20:23 +0800

    Dependency Injection Ninject

--- XEngine.Web/Controllers/HomeController.cs
+++ XEngine.Web/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Ninject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using XEngine.Web.DAL;
+using XEngine.Web.Models;
 
 namespace XEngine.Web.Controllers
 {
@@ -14,10 +16,11 @@ namespace XEngine.Web.Controllers
 
         public ActionResult Index()
         {
-            var user = db.SysRoles.Count();
+            //var user = db.SysRoles.Count();
+
             return View();
         }
-        
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -28,6 +31,24 @@ namespace XEngine.Web.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Battle()
+        {
+            //Samurai model = new Samurai(new Sword());
+            //ViewBag.Hit = model.Hit("Scott");
+
+            //1. 创建一个Ninject的内核实例
+            IKernel ninjectKernel = new StandardKernel();
+            //2. 配置Ninject内核,指明接口需绑定的类
+            ninjectKernel.Bind<IWeapon>().To<Sword>();
+            //3. 根据上一步的配置创建一个对象
+            var weapon = ninjectKernel.Get<IWeapon>();
+
+            var warrior1 = new Samurai(weapon);
+            ViewBag.Hit = warrior1.Hit("the evildoers");
 
             return View();
         }
